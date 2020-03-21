9ffa979949c16e8e348945a1a330a8f27676bf69
alex2018hillel <alex2018hillel@gmail.com>
2020-03-21 14:39:35 +0200

    add joke.py

---
+++ joke.py
@@ -0,0 +1,32 @@
+import telebot, bs4, requests
+bot = telebot.TeleBot("804665851:AAFMVbf4aPK5ANVk3GhE0q42m3hIP-JR10E")
+
+def getanekdot():
+    z=''
+    s=requests.get('http://anekdotme.ru/random')
+    b=bs4.BeautifulSoup(s.text, "html.parser")
+    p=b.select('.anekdot_text')
+    for x in p:
+        s=(x.getText().strip())
+        z=z+s+'\n\n'
+    return s
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    msg=message.text
+    msg=msg.lower()
+    if (u'анекдот' in msg):
+        try:
+            bot.send_message(message.from_user.id, getanekdot())
+        except:
+            pass
+    else:
+        bot.send_message(message.from_user.id, u'Даша, напишите мне слово Анекдот')
+
+bot.polling(none_stop=True, timeout=123)
+
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message):
+    bot.send_message(message.from_user.id, u'Даша, напишите мне слово Анекдот')
+
+bot.polling(none_stop=True, interval=0)
